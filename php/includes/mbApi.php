6b7d07774dc49bb8d0fde1bdb544e6de6e9ad8e4
mattbutlar <matt.butlar@mindbodyonline.com>
2012-09-21 09:24:25 -0700

    Fix for namespace

--- php/includes/mbApi.php
+++ php/includes/mbApi.php
@@ -13,7 +13,7 @@ function GetApiHostname()
 
 function GetApiNamespace()
 {
-	return "http://clients.mindbodyonline.com/0_5";
+	return "http://clients.mindbodyonline.com/api/0_5";
 }
 
 // helper functions
