1fa57e37bd7cf2b689b91efc78eb666b1e76443c
mattbutlar <matt.butlar@mindbodyonline.com>
2012-10-04 10:43:17 -0700

    Ruby Examples

---
+++ Ruby/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Path::Application.config.secret_token = '778ce0a27eb9fda05c08dabb8ceef1de3301ab6ccf3941f17d21bb7ce763f62a5327100e6634f661c54bf7022b8d666812b217164047ecdc06bdc550e1cdc985'
